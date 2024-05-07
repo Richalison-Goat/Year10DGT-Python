319d350ce608c84e2c5d416602f2c3ef3ab13971
Richalison-Goat <168597077+Richalison-Goat@users.noreply.github.com>
2024-05-08 10:04:09 +1200

    Add files via upload
    functions go here .py

---
+++ #functions go here.py
@@ -0,0 +1,11 @@
+#functions go here
+def intro():
+    print("welcome")
+    print()
+    print("Instructions go here")
+    return"" #I am returning all items
+                # in the code 
+
+# main routine 
+intro() # This calls the function so that we can use it 
+    
